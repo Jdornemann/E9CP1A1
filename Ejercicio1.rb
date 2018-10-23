cbcf3c86707b1dccbd2c5e5b7bcaa8806ac46a65
jdornemann <johann.dornemann.c@gmail.com>
2018-10-23 19:24:01 -0300

    primer ejercicio

---
+++ Ejercicio1.rb
@@ -0,0 +1,21 @@
+# Ejercicio 1: Sintaxis.
+
+class Anything
+    attr_accessor :a
+    def initialize
+        @a = 0
+    end
+
+    def foo
+      a = 5
+    end
+  
+    def bar
+      @a += 1
+    end
+  end
+  
+  any = Anything.new
+  puts any.foo
+  puts any.bar
+  puts any.a
